4810ce771200298918c6508f59e819e6f1c2cf64
gslee <gyuseob.lee@myrealtrip.com>
2021-09-04 23:43:36 +0900

    remove none import

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.parser.Entity;
 
 @RestController
 public class HelloController {
